e4ada7a2be3d24f8f978254c4592195020f649d4
AuraMarcus-sys <marcus.hill503@gmail.com>
2026-07-03 16:56:41 -0700

    Create traffic_light.v

---
+++ traffic_light.v
@@ -0,0 +1,71 @@
+module traffic_light(
+    input clk,
+    input reset,
+
+    output reg [2:0] north,
+    output reg [2:0] east
+);
+
+parameter RED    = 3'b100;
+parameter YELLOW = 3'b010;
+parameter GREEN  = 3'b001;
+
+// States
+parameter S0 = 2'b00;
+parameter S1 = 2'b01;
+parameter S2 = 2'b10;
+parameter S3 = 2'b11;
+
+reg [1:0] state;
+
+always @(posedge clk or posedge reset)
+begin
+
+    if(reset)
+        state <= S0;
+    else
+        case(state)
+
+            S0: state <= S1;
+            S1: state <= S2;
+            S2: state <= S3;
+            S3: state <= S0;
+
+        endcase
+
+end
+
+always @(*)
+begin
+
+    case(state)
+
+        S0:
+        begin
+            north = GREEN;
+            east  = RED;
+        end
+
+        S1:
+        begin
+            north = YELLOW;
+            east  = RED;
+        end
+
+        S2:
+        begin
+            north = RED;
+            east  = GREEN;
+        end
+
+        S3:
+        begin
+            north = RED;
+            east  = YELLOW;
+        end
+
+    endcase
+
+end
+
+endmodule
